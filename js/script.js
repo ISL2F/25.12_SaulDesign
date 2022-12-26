384d3747298e0b5ac01b7be67f2e337a17ea5393
ISL2F <98880412+ISL2F@users.noreply.github.com>
2022-12-26 20:21:43 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+const tabNavItems = document.querySelectorAll(".buttons-tab");
+const tabItems = document.querySelectorAll(".tab");
+document.addEventListener("click", function (e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if (targetElement.closest(".buttons-tab")) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains("active")) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove("active");
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add("active");
+    tabItems[currentActiveIndex].classList.remove("active");
+    tabItems[newActiveIndex].classList.add("active");
+  }
+});
+
+/*window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+  document.addEventListener("click", documentActions);
+}
+function documentActions(e) {
+  const targetElement = e.target;
+  if (targetElement.hasAttribute("data-goto")) {
+    const gotoElement = document.querySelector(`${targetElement.dataset.goto}`);
+    const headerHeight = document.querySelector(`.header`).offsetHeight;
+    if (gotoElement) {
+      window.scrollTo({
+        top: gotoElement.offsetTop - headerHeight,
+        behavior: "smooth",
+      });
+    }
+    e.preventDefault;
+  }
+} */
